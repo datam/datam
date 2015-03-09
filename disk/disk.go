282a20054e7c74948935395505a379198a5ab1b6
Data M <datam@mailinator.com>
2015-03-08 23:06:50 -0700

    Open/Create/Read/Write

--- disk/disk.go
+++ disk/disk.go
@@ -3,12 +3,15 @@ package disk
 import "os"
 import "errors"
 
-var InvalidBuffer = errors.New("Buffer not expected size")
+var errInvalidBuffer = errors.New("Buffer not expected size")
+var errInvalidDiskSize = errors.New("Disk length aligned to block size")
+var errInvalidBlockNumber = errors.New("Block Number is beyond max length")
+var errInsufficientBufferSpace = errors.New("Buffer provided is not the expected length")
 
 const DefaultBlockSize = 4096
 
 type Disk interface {
-	BlockSize() uint32
+	BlockSize() uint64
 	NumBlocks() uint64
 	Read(b uint64, p []byte) error
 	Write(b uint64, p []byte) error
@@ -19,21 +22,55 @@ type disk struct {
 	f         *os.File
 	length    uint64
 	numBlocks uint64
-	blockSize uint32
+	blockSize uint64
 	path      string
 	Disk
 }
 
-func OpenDisk(p string, numBlocks uint64) (d *disk, err error) {
+func CreateDisk(p string, numBlocks uint64, blockSize uint64) (d *disk, err error) {
+	f, err := os.Create(p)
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+
+	err = f.Truncate(int64(numBlocks * blockSize))
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+
+	f.Close()
+
+	return OpenDisk(p)
+}
+
+func OpenDisk(p string) (d *disk, err error) {
 	d = new(disk)
 	d.path = p
-	d.numBlocks = numBlocks
 	d.blockSize = DefaultBlockSize
-	d.f, err = os.OpenFile(p, os.O_RDWR|os.O_CREATE, 0)
+
+	d.f, err = os.OpenFile(p, os.O_RDWR, 0)
 	if err != nil {
 		return nil, err
 	}
 
+	fileInfo, err := d.f.Stat()
+	if err != nil {
+		d.f.Close()
+		return nil, err
+	}
+
+	d.length = uint64(fileInfo.Size())
+	if err != nil {
+		return nil, err
+	}
+
+	if d.length%d.blockSize != 0 {
+		return nil, errInvalidDiskSize
+	}
+
+	d.numBlocks = d.length / d.blockSize
 	return d, nil
 }
 
@@ -41,18 +78,44 @@ func (d *disk) NumBlocks() (numBlocks uint64) {
 	return d.numBlocks
 }
 
-func (d *disk) BlockSize() (blockSize uint32) {
+func (d *disk) BlockSize() (blockSize uint64) {
 	return d.blockSize
 }
 
 func (d *disk) Read(b uint64, p []byte) error {
+	if b > d.numBlocks {
+		return errInvalidBlockNumber
+	}
+
+	if len(p) < int(d.blockSize) {
+		return errInsufficientBufferSpace
+	}
+
+	_, err := d.f.ReadAt(p, int64(d.blockSize*b))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (d *disk) Write(b uint64, p []byte) error {
+	if b > d.numBlocks {
+		return errInvalidBlockNumber
+	}
+
+	if len(p) != int(d.blockSize) {
+		return errInsufficientBufferSpace
+	}
+
+	_, err := d.f.WriteAt(p, int64(b))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (d *disk) Close() error {
-	return nil
+	return d.f.Close()
 }
--- disk/disk_test.go
+++ disk/disk_test.go
@@ -1,11 +1,39 @@
 package disk
 
 import "testing"
+import "os"
+
+const numBlocks = 1000
+const blockSize = 4096
+const filePath = "/tmp/disk.file"
 
 func TestDiskCreate(t *testing.T) {
-	var d, err = OpenDisk("/tmp/disk.file", 1000)
+	var d, err = CreateDisk(filePath, numBlocks, blockSize)
 
 	if d == nil || err != nil {
 		t.Fatalf("Failed to create disk!")
 	}
+
+	d.Close()
+
+	d, err = OpenDisk(filePath)
+
+	if d == nil || err != nil {
+		t.Fatalf("Failed to open disk!")
+	}
+
+	if d.NumBlocks() != numBlocks {
+		t.Fatalf("numBlocks mismatch!")
+	}
+
+	d.Close()
+
+	err = os.Remove("/tmp/disk.file")
+	if err != nil {
+		t.Fatalf("Failed to remove disk file after test")
+	}
+}
+
+func TestDiskReadWrite(t *testing.T) {
+
 }
